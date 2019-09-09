64dfaedcc7e87e62c559a429b3cd4d792e97fb4d
João Vitor Machado <joaovmmachado@gmail.com>
2019-09-09 18:38:31 -0300

    add slugs and replace url id number for product slug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 
 function replaceTemplate(temp, product) {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
@@ -11,6 +12,7 @@ function replaceTemplate(temp, product) {
   output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%SLUG%}/g, product.slug);
 
   if (!product.organic)
     output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
@@ -34,6 +36,10 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+
+dataObj.forEach((el, i) => (el.slug = slugs[i]));
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
@@ -49,8 +55,8 @@ const server = http.createServer((req, res) => {
     // Product page
   } else if (pathname === '/product') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-
-    const product = dataObj[query.id];
+    const id = dataObj.findIndex(el => el.slug === query.id);
+    const product = dataObj[id];
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
